1fd5baff335215ea1af74051cbd75ed721d753d6
IronFist482 <icernap2000@alumno.ipn.mx>
2023-03-29 16:24:15 -0600

    DOCS: Creación de screens y modificación en MainNavigator // 100%

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,5 +1,118 @@
 // AQUI VA EL MAIN NAVIGATOR, AQUI METERAN REACT NAVIGATION Y LLAMARAN LOS COMPONENTES DE @screens
-
-import App from "@screens/Main/Main"
-
-export default App
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Ionicons,
+  Entypo,
+} from "@expo/vector-icons";
+import { Text } from "react-native";
+// Screens
+import HomeScreen from "@screens/Home/Home";
+import MainScreen from "@screens/Main/Main";
+import ConfigScreen from "@screens/Config/Config";
+import FrequentsScreen from "@screens/Frequents/Frequents";
+import AnalyticsScreen from "@screens/Analytics/Analytics";
+const Tab = createBottomTabNavigator();
+export default function MainNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "#044C7C" : "#0003" }}>
+                Inicio
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name="home"
+                size={30}
+                style={{ color: focused ? "#044C7C" : "#0003" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Analytics"
+          component={AnalyticsScreen}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "#044C7C" : "#0003" }}>
+                Análisis
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <Entypo
+                name="bar-graph"
+                size={24}
+                style={{ color: focused ? "#044C7C" : "#0003" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Frequents"
+          component={FrequentsScreen}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "#044C7C" : "#0003" }}>
+                Frecuentes
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="time"
+                size={24}
+                color="black"
+                style={{ color: focused ? "#044C7C" : "#0003" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Config"
+          component={ConfigScreen}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "#044C7C" : "#0003" }}>
+                Config
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <MaterialIcons
+                name="settings"
+                size={24}
+                color="black"
+                style={{ color: focused ? "#044C7C" : "#0003" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Signout"
+          component={MainScreen}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "#044C7C" : "#0003" }}>
+                Salir
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <MaterialIcons
+                name="logout"
+                size={24}
+                color="black"
+                style={{ color: focused ? "#044C7C" : "#0003" }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
